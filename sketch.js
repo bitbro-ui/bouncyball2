ed11c776103c5bb37de6af6c2cf672a7f00b191d
bitbro-ui <52121125+bitbro-ui@users.noreply.github.com>
2019-12-28 18:53:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var fixedRect ,movingRect ;
+var bg_color,mouse_ellipse;
+bg_color = mouse_ellipse;
+
+mouse_ellipse = 
+
+function setup() {
+  background(bg_color);
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+
+  bounceOff(movingRect,fixedRect);
+  drawSprites();
+}
